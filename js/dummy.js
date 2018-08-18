05399a0bf860012751dd4b4393f738c293ca3026
Mohana Naga Venkat <mohananagavenkat@gmail.com>
2018-08-18 21:36:30 +0530

    Added ESLINT - Linting

---
+++ js/dummy.js
@@ -0,0 +1,38 @@
+const _ = function (id) {
+  return document.getElementById(id);
+};
+
+const content = _('content');
+
+// console.log(content.innerHTML);
+
+const color = _('color');
+const fs = _('font-size');
+const bold = _('bold');
+const underline = _('underline');
+const italic = _('italic');
+
+const changeContentStyle = function (prop, value) {
+  content.style[prop] = value;
+};
+
+color.addEventListener('input', function () {
+  changeContentStyle('color', `#${this.value}`);
+  _('color-preview').style.background = `#${this.value}`;
+});
+
+fs.addEventListener('input', function () {
+  changeContentStyle('fontSize', `${this.value}px`);
+});
+
+bold.addEventListener('click', function () {
+  return this.checked ? changeContentStyle('fontWeight', 'bold') : changeContentStyle('fontWeight', 'normal');
+});
+
+underline.addEventListener('click', function () {
+  return this.checked ? changeContentStyle('textDecoration', 'underline') : changeContentStyle('textDecoration', 'none');
+});
+
+italic.addEventListener('click', function () {
+  return this.checked ? changeContentStyle('fontStyle', 'italic') : changeContentStyle('fontStyle', 'normal');
+});
--- js/gulpfile_for_autoprefixer.js
+++ js/gulpfile_for_autoprefixer.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 const gulp = require('gulp');
 
 const sass = require('gulp-sass');
@@ -6,22 +7,20 @@ const autoprefixer = require('gulp-autoprefixer');
 
 const maps = require('gulp-sourcemaps');
 
-gulp.task('sass',function(){
-    return gulp.src('./sass/**/*.scss')
-           .pipe(maps.init())
-           .pipe(sass())
-           .pipe(maps.write('./maps'))
-           .pipe(gulp.dest('./css'))
-});
+/* eslint-enable */
 
-gulp.task('build',function(){
-    return gulp.src('./css/**/*.css')
-            .pipe(autoprefixer({
-                browsers: ['last 8 versions', 'safari 5', 'ie 6', 'ie 7', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4']
-            }))
-            .pipe(gulp.dest('./build'));
-});
+gulp.task('sass', () => gulp.src('./sass/**/*.scss')
+  .pipe(maps.init())
+  .pipe(sass())
+  .pipe(maps.write('./maps'))
+  .pipe(gulp.dest('./css')));
 
-gulp.task('watch',function(){
-    gulp.watch('./sass/**/*.scss',['sass']);
+gulp.task('build', () => gulp.src('./css/**/*.css')
+  .pipe(autoprefixer({
+    browsers: ['last 8 versions', 'safari 5', 'ie 6', 'ie 7', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'],
+  }))
+  .pipe(gulp.dest('./build')));
+
+gulp.task('watch', () => {
+  gulp.watch('./sass/**/*.scss', ['sass']);
 });
