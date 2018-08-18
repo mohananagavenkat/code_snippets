93f99b92ae47d525a9cab362cb98958ad27d9e45
Mohana Naga Venkat <mohananagavenkat@gmail.com>
2018-08-18 21:48:13 +0530

    Linted gulpfile_for_sass

--- js/gulpfile_for_sass.js
+++ js/gulpfile_for_sass.js
@@ -1,17 +1,19 @@
+/* eslint-disable */
+
 const gulp = require('gulp');
 
 const sass = require('gulp-sass');
 
 const maps = require('gulp-sourcemaps');
 
-gulp.task('sass',function(){
-    return gulp.src('./sass/**/*.scss')
-           .pipe(maps.init())
-           .pipe(sass())
-           .pipe(maps.write('./maps'))
-           .pipe(gulp.dest('./css'))
-});
+/* eslint-enable */
 
-gulp.task('watch',function(){
-    gulp.watch('./sass/**/*.scss',['sass']);
+gulp.task('sass', () => gulp.src('./sass/**/*.scss')
+  .pipe(maps.init())
+  .pipe(sass())
+  .pipe(maps.write('./maps'))
+  .pipe(gulp.dest('./css')));
+
+gulp.task('watch', () => {
+  gulp.watch('./sass/**/*.scss', ['sass']);
 });
